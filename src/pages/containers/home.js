6c0776ab21fb5ce1f224bf3159268a0142d94341
John serrano <andrey_201015@hotmail.com>
2018-04-10 11:13:46 -0500

    Manejando errores

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HomeLayout from '../components/home-layout';
 import Tasks from '../../tasks/containers/tasks';
+import HandleError from '../../error/containers/errores';
 class Home extends Component {
   constructor(props) {
     super(props)
@@ -11,9 +12,11 @@ class Home extends Component {
 
   render() {
     return (
-      <HomeLayout>
-        <Tasks name={this.props.name}/>
-      </HomeLayout>
+      <HandleError>
+        <HomeLayout>
+          <Tasks name={this.props.name}/>
+        </HomeLayout>
+      </HandleError>
     )
   }
 }
